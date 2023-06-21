0507c1ff88358b981ca5808cfc714220cc590706
Thiago-Cez <thiago.silva.ts804@gmail.com>
2023-06-21 23:26:09 +0100

    class12

---
+++ class12/class12.js
@@ -0,0 +1,33 @@
+for(contador = 0; contador < 10; contador++) {
+    console.log("oi")
+}
+//-----------------------------------------------------
+for(contador = 1; contador <= 10; contador++) {
+    console.log("oi" + contador)
+}
+
+
+// faça um for que imprima os numeros de 1 a 10 
+
+for(imprimirNumeros = 0; imprimirNumeros <= 10; imprimirNumeros++) {
+    console.log(imprimirNumeros)    
+};
+
+// faça um for que imprima os numeros pares entre 0 a 50 (excluindo 0 e 50)
+
+for(c = 2; c <= 50; c += 2) {
+    console.log(c)
+}
+
+//faça um for que imprima os numeros impares entre 0 a 50 (excluindo 0 e 50)
+
+for( c = 3; c <= 50; c += 3){
+    console.log(c)
+}
+
+
+// faça um for que imprima os numeros multiplos de 3 entre 0 a 50 (excluindo 0 e 50 );
+
+for (let c = 3; c <= 50; c += 3) {
+    console.log(c)
+}
