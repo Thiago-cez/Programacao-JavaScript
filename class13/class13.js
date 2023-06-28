9a759a17743d85d0164205b0de92894ec333748d
Thiago-Cez <thiago.silva.ts804@gmail.com>
2023-06-28 23:53:30 +0100

    terminar exercicos class13

--- class13/class13.js
+++ class13/class13.js
@@ -14,8 +14,8 @@ console.log("O loop foi realizador por "+ c + " vezes")
 /** 
  * Faça um programa que leia uma quantidade n de números,  some a quantidade de números pares e ímpares digitados e 
  * exiba no final o resultado 
- */
-
+ 
+                                                   FINALIZADO!!!
 let sair = "nao";
 let c = 0 ;
 let par = 0;
@@ -37,7 +37,7 @@ while (sair == "nao") {
     console.log("o loop foi realizado por " + c + " vezes")
     console.log("o numero par foi impresso " + par + " vezes")
     console.log("o numero impar foi impresso " + impar + "vezes")
-
+*/
 //-------------------------------------------------------------
 
 
@@ -46,13 +46,13 @@ while (sair == "nao") {
  * - Faça um programa que leia uma quantidade n de números,  some a quantidade de números pares e ímpares digitados e 
  * calcula a média de todos os números digitados, exiba no final o resultado 
  * 
-  */
+  
  let out = "nao";
  let cont = 0; 
  let numerosImpares = 0;
  let numerosPares = 0;
  let media;
-
+                                                       FINALIZADO!!!!!
 while (out == "nao") {
     let num =  prompt("digite um numero inteiro")
     if(num%2 == 0){
@@ -70,10 +70,8 @@ while (out == "nao") {
 console.log("media dos numeros Calculados é: "+ media);
 console.log("a quantidade de numeros pares são: "+ numerosPares);
 console.log("a quantidade de numeros Impares São: "+ numerosImpares);
-
-
-
 //-------------------------------------------------------------
+*/
 
 
 
@@ -83,11 +81,29 @@ console.log("a quantidade de numeros Impares São: "+ numerosImpares);
  * 
 */
 
+var numerosPares = 0;
+var somarPares = 0;
+var numerosImpares = 0;
+var somarImpares = 0;
+var saida = "nao";
 
+while(saida == "nao") {
+    var num = prompt("Digite um numero inteiro");
+    var media = somarImpares + somarPares / 2;
 
-
-
-
+    if(num%2==0){
+        numerosPares = numerosPares + 1;
+        somarPares = somarPares + parseFloat(num);
+        console.log(somarPares)
+    }
+    else {
+        numerosImpares = numerosImpares + 1;
+        somarImpares = somarImpares + parseFloat(num);
+        console.log(somarImpares);
+    }
+    saida = prompt("Deseja sair??!  SIM | NAO ")
+}
+console.log(" A média dos números digitados é: " + media)
 
 
 
@@ -110,4 +126,5 @@ console.log("a quantidade de numeros Impares São: "+ numerosImpares);
 //deseja sair? nao
 //digite um numero:5 (5x1=5, 5x2=10 ... 5x10=50)
 //deseja sair? sim
-   
+
+//------------------------------------------------------------  
