3bcb8e28bc5676916371ba764480aa6706b1ddb9
Thiago-Cez <thiago.silva.ts804@gmail.com>
2023-07-05 23:39:31 +0100

    class16

---
+++ class16/class16.js
@@ -0,0 +1,31 @@
+/* console.log(" class 16 ");
+
+let notas = [ 7.5, 2.3, 5.6, 7.8, 9.0, 10 ];
+
+console.log(" Quantidade de Notas: " + notas.length);
+console.log("nota 1: " + notas[0]);
+console.log("nota 2: " + notas[1]);
+console.log("nota 3: " + notas[2]);
+console.log("nota 4: " + notas[3]);
+console.log("nota 5: " + notas[4]);
+console.log("nota 6: " + notas[5]);
+*/
+
+
+let notas = [ 7.5, 2.3, 5.6, 7.8, 9.0, 10 ];
+
+console.log(" Quantidade de Notas: " + notas.length);
+
+let i = 0;
+
+console.log("nota 1: " + notas[i]);
+i++
+console.log("nota 2: " + notas[i]);
+i++
+console.log("nota 3: " + notas[i]);
+i++
+console.log("nota 4: " + notas[i]);
+i++
+console.log("nota 5: " + notas[i]);
+i++
+console.log("nota 6: " + notas[i]);
