9e223149cbf219df65c8b88cb98a6cce133a7fbd
Thiago-Cez <thiago.silva.ts804@gmail.com>
2023-07-10 00:52:23 +0100

    class16

---
+++ class16/challenge16/challenge16.js
@@ -0,0 +1,7 @@
+//1) faça um array e armazene 20 números manualmente, exiba todos eles na tela com um loop FOR.
+
+let arreyNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+
+for(let i = 0; i < arreyNumeros.length; i++){
+    console.log(`nota ${i+1} = ${arreyNumeros[i]}`);
+}
--- class16/class16.js
+++ class16/class16.js
@@ -11,21 +11,32 @@ console.log("nota 5: " + notas[4]);
 console.log("nota 6: " + notas[5]);
 */
 
-
+//-------------------------------------------------------------------------------------
+/*
 let notas = [ 7.5, 2.3, 5.6, 7.8, 9.0, 10 ];
 
-console.log(" Quantidade de Notas: " + notas.length);
+console.log("QTD:"+ notas.length)
+let i = 0
+console.log("Nota 1: " +notas[i])  //i=0  notas[0]
+i++ // i=i+1  //i=0+1 //1
+console.log("Nota 2: "+ notas[i])   //i=1  notas[1]
+i++  //i=2
+console.log("Nota 3: "+ notas[i])   //i=2 notas[2]
+i++ //i=3
+console.log("Nota 4: "+ notas[i])   //i=3 notas[3]
+i++ //i=4
+console.log("Nota 5: "+ notas[i])  //i=4 notas[4]
+i++ //i=5
+console.log("Nota 6: "+ notas[i])  //i=5 notas[5]
 
-let i = 0;
+*/
 
-console.log("nota 1: " + notas[i]);
-i++
-console.log("nota 2: " + notas[i]);
-i++
-console.log("nota 3: " + notas[i]);
-i++
-console.log("nota 4: " + notas[i]);
-i++
-console.log("nota 5: " + notas[i]);
-i++
-console.log("nota 6: " + notas[i]);
+//--------------------------------------------------------------------------------------
+
+let notas = [7.5, 2.3, 5.6, 7.8, 9.0, 10, 7.8, 9.7, 6.5];
+
+console.log("QTD:"+ notas.length);
+
+for (let i = 0; i< notas.length;i++ ) {
+    console.log(`Nota ${i+1} :  ${notas[i]}`) 
+}
