30715e75d0dd18e7212b5a2950d08bd05bca258a
Thiago-Cez <thiago.silva.ts804@gmail.com>
2023-07-13 00:48:21 +0100

    class16 - complete

--- class16/challenge16/challenge16.js
+++ class16/challenge16/challenge16.js
@@ -61,6 +61,7 @@ console.log( "o Maior numero é: "+ maiorNumero);
 
 //5) faça um array e armazene 10 números, descubra e exiba o menor número do array
 
+/*
 let arreyNumeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 let menorNumero = Infinity;
 
@@ -72,3 +73,24 @@ for( i = 0; i < arreyNumeros.length; i++ ){
   }
 }
 console.log(" O menor numero é: " + menorNumero);
+
+*/
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// 6) faça uma array e armazene 10 números, calcule e exiba a média dos  numeros do array;
+
+let arreyNumeros = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let qtdVezes = 0;
+let somaNumeros = 0;
+
+
+for (i = 0; i < arreyNumeros.length; i++) {
+  somaNumeros = arreyNumeros[i] + somaNumeros;
+  qtdVezes++
+}
+let media = somaNumeros / qtdVezes;
+
+
+
+console.log(" A media da soma desse Arrey é: " + media);
